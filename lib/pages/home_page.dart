0405b59de93a27f00c6993026df0079d40f9f931
Luka Jincharadze <lukajinchara@gmail.com>
2023-04-01 20:49:52 +0400

    commit
    todolist update, listing strings

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final todoInputController = TextEditingController();
+  final List<String> todos = [];
+
+  List<Widget> getTodoWidgets() {
+    List<Widget> temp = [];
+
+    for (int i = 0; i< todos.length; i++) {
+      temp.add(Text(todos[i]));
+    }
+    return temp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Todo"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 200,
+                child: TextField(
+                  controller: todoInputController,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    todos.add(todoInputController.text);
+                    todoInputController.clear();
+                  });
+                },
+                child: Text("Add"),
+              )
+            ],
+          ),
+          Container(
+            height: 200,
+            child: ListView(
+              children: getTodoWidgets(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
